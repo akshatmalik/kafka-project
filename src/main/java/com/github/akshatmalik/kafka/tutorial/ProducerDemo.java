88398ab1cb8abdc89c5cd8ff46dacef12bded1d8
Akshat Malik <akshat.ndun@gmail.com>
2020-04-01 18:11:10 +0530

    Initial Commit

---
+++ src/main/java/com/github/akshatmalik/kafka/tutorial/ProducerDemo.java
@@ -0,0 +1,44 @@
+package com.github.akshatmalik.kafka.tutorial;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemo.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10 ; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello: " + i);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received new metadata! \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error occurred! ", e);
+                    }
+                }
+            });
+        }
+
+
+        producer.flush();
+        producer.close();
+    }
+}
